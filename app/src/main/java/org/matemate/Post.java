b275a46ecbdc2337a6d104d959f340d5e6f45331
dayeondayeon <dayeon2438@gmail.com>
2021-07-31 01:23:45 +0900

    Feat : 메인 레이아웃 구현

---
+++ app/src/main/java/org/matemate/Post.java
@@ -0,0 +1,68 @@
+package org.matemate;
+
+public class Post {
+    String name;
+    String title;
+    String location;
+    int curNum;
+    int minNum;
+    boolean closed;
+
+    public Post(String name, String title, String location, int curNum, int minNum, boolean closed) {
+        this.name = name;
+        this.title = title;
+        this.location = location;
+        this.curNum = curNum;
+        this.minNum = minNum;
+        this.closed = closed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getCurNum() {
+        return curNum;
+    }
+
+    public void setCurNum(int curNum) {
+        this.curNum = curNum;
+    }
+
+    public int getMinNum() {
+        return minNum;
+    }
+
+    public void setMinNum(int minNum) {
+        this.minNum = minNum;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    public void setClosed(boolean closed) {
+        this.closed = closed;
+    }
+
+}
---
+++ app/src/main/java/org/matemate/PostAdapter.java
@@ -0,0 +1,45 @@
+package org.matemate;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
+        ArrayList<Post> posts = new ArrayList<Post>();
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+                public ViewHolder(@NonNull View itemView) {
+                        super(itemView);
+                }
+
+                public void setPost(Post post) {
+
+                }
+        }
+
+        @NonNull
+        @Override
+        public PostAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+                LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+                View postView = inflater.inflate(R.layout.activity_main, viewGroup, false);
+                return new ViewHolder(postView);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull PostAdapter.ViewHolder holder, int position) {
+                Post post = posts.get(position);
+                holder.setPost(post);
+        }
+
+        @Override
+        public int getItemCount() {
+                return 0;
+        }
+
+
+}
