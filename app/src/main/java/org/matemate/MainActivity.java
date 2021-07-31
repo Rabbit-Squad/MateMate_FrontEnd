9374e8b94a5be0d4722733c793d9cbf0662409ce
dayeondayeon <dayeon2438@gmail.com>
2021-08-01 00:06:46 +0900

    홈화면 menu 연결

--- app/src/main/java/org/matemate/MainActivity.java
+++ app/src/main/java/org/matemate/MainActivity.java
@@ -1,10 +1,16 @@
 package org.matemate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +23,16 @@ public class MainActivity extends AppCompatActivity {
         LinearLayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(manager);
         PostAdapter adapter = new PostAdapter();
+
+        ImageView menu = findViewById(R.id.menu_button);
+        final DrawerLayout drawer = findViewById(R.id.main_layout);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawer.openDrawer(Gravity.LEFT);
+            }
+        });
+
 
         adapter.addPost(new Post("피바", "밥 먹읍시다. ", "작은도쿄", 1,2, "15:30",false));
         adapter.addPost(new Post("쭈니", "어차피", "윈도", 1, 8, "21:00", true));
