277ae41856066f75b2cfed36f5cb088679cc1d17
dayeondayeon <dayeon2438@gmail.com>
2021-08-06 01:28:08 +0900

    Feat : 회원가입 id, pw, email 유효성 검사

---
+++ app/src/main/java/org/matemate/SignUpActivity.java
@@ -0,0 +1,63 @@
+package org.matemate;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
+import java.util.regex.Pattern;
+
+public class SignUpActivity extends AppCompatActivity {
+    EditText id;
+    EditText pw;
+    EditText nickname;
+    EditText email;
+    String num_eng_pattern = "^[a-zA-Z0-9]{8~15}$"; // 영문+숫자+8~15자
+    String email_pattern = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        id = findViewById(R.id.id_input);
+        pw = findViewById(R.id.pw_input);
+        nickname = findViewById(R.id.nickname_input);
+        email = findViewById(R.id.email_input);
+
+        id.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus) {
+                    if(!num_eng_pattern.matches((id.getText()).toString())) {
+                        Toast.makeText(id.getContext(), "아이디는 영문과 숫자조합의 8~15자여야 합니다. ", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        pw.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus) {
+                    if(!num_eng_pattern.matches((pw.getText()).toString())) {
+                        Toast.makeText(pw.getContext(), "비밀번호는 영문과 숫자조합의 8~15자여야 합니다. ", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        email.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus) {
+                    if(!email_pattern.matches((email.getText()).toString())) {
+                        Toast.makeText(email.getContext(), "유효한 이메일주소를 입력하세요. ", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+}
