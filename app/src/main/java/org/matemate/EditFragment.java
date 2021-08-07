122a52e9c3efaa522c8767b8912639b541ddb086
dayeondayeon <dayeon2438@gmail.com>
2021-08-08 03:57:47 +0900

    Design : 글 작성화면 spinner 추가

--- app/src/main/java/org/matemate/EditFragment.java
+++ app/src/main/java/org/matemate/EditFragment.java
@@ -9,7 +9,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.Calendar;
 
 public class EditFragment extends Fragment {
 
@@ -20,10 +24,8 @@ public class EditFragment extends Fragment {
     EditText text_input;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_edit, container, false);
-
         initUI(rootView);
 
         return rootView;
