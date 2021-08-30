351637e4dec490a2bdb080fc1d633eb8a14bf734
dayeondayeon <dayeon2438@gmail.com>
2021-08-31 01:44:53 +0900

    Refactor : local change병합

--- app/src/main/java/org/matemate/EditFragment.java
+++ app/src/main/java/org/matemate/EditFragment.java
@@ -1,5 +1,7 @@
 package org.matemate;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,15 +9,26 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.util.EventLog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.Calendar;
+import java.util.EventListener;
 import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class EditFragment extends Fragment {
 
